51bcc9890485b85c4926319c61681b8f305940ba
Markus Flingelli <markus.flingelli@gmx.de>
2025-05-14 20:17:51 +0200

    Anzahl richtige Antworten ergänzt.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let score = 0;
+
 function checkAnswer(questionId, hint, correct) {
-    document.getElementById(`result_${questionId}`).innerText = correct ? "Richtig! ${hint}" : "Falsch!";
+	if (correct) {
+        score += 1;
+    }
+    document.getElementById(`result_${questionId}`).innerText = correct ? "Richtig! " + hint : "Falsch!";
+	document.getElementById("score").innerText = `Richtige Antworten: ${score}`;
 }
